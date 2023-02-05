94f6faa4b57552d607575baba68dd0d884e382bc
yumerov <yumerov.levent@gmail.com>
2023-02-06 00:04:34 +0200

    [Data] database config

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'driver' => 'pdo_pgsql',
+    'port' => '5432',
+    'host' => 'localhost',
+    'database' => 'hospitals',
+    'user' => 'postgres',
+    'password' => 'postgres',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+];
